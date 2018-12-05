247b3c9cc32628fb693f3678a91742db82ad38b7
Pixelynx <tjstevens1992@gmail.com>
2018-12-05 10:46:08 -0500

    math add done

---
+++ utility_server_LAB/math_route.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+// app.use(express.static('public'));
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json());
+
+app.get('/math/add/', (req, res) => {
+  const query = res.query;
+  let num1 = req.query.num1;
+  let num2 = req.query.num2;
+  // console.log(num1.params.query)
+  if (!isNaN(num1) && !isNaN(num2)) {
+    console.log(!isNaN(num1))
+
+    let data = parseInt(num1) + parseInt(num2);
+    res.json({ input: req.query, sumString: `${req.query.num1} + ${req.query.num2}`, sum: data });
+  } else {
+    res.send(`Please enter a valid number.`);
+  }
+
+});
+
+app.listen(3000, () => {
+  console.log('Listening to port 3000.')
+});
